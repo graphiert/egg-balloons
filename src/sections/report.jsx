ae153d81c3b5d298c2b41cad9fd082b1bc6f28fc
Galih Puji Irianto <galihpujiirianto@gmail.com>
2025-05-12 21:14:10 +0700

    Add more paragraph on explanation section

--- src/sections/report.jsx
+++ src/sections/report.jsx
@@ -7,12 +7,17 @@ export default function Intro() {
         <Card title="Pembahasan praktikum">
           <p className="text-pretty text-sm">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus eos deleniti totam hic aperiam amet, aspernatur laboriosam ex repudiandae quod, praesentium saepe a molestias tempora. Eligendi corporis quas, debitis animi, mollitia iste odit eveniet sapiente soluta aliquam magni provident inventore sequi in itaque, aspernatur voluptatum suscipit veritatis assumenda ducimus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus eos deleniti totam hic aperiam amet, aspernatur laboriosam ex repudiandae quod, praesentium saepe a molestias tempora. Eligendi corporis quas, debitis animi, mollitia iste odit eveniet sapiente soluta aliquam magni provident inventore sequi in itaque, aspernatur voluptatum suscipit veritatis assumenda ducimus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus eos deleniti totam hic aperiam amet, aspernatur laboriosam ex repudiandae quod, praesentium saepe a molestias tempora. Eligendi corporis quas, debitis animi, mollitia iste odit eveniet sapiente soluta aliquam magni provident inventore sequi in itaque, aspernatur voluptatum suscipit veritatis assumenda ducimus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus eos deleniti totam hic aperiam amet, aspernatur laboriosam ex repudiandae quod, praesentium saepe a molestias tempora. Eligendi corporis quas, debitis animi, mollitia iste odit eveniet sapiente soluta aliquam magni provident inventore sequi in itaque, aspernatur voluptatum suscipit veritatis assumenda ducimus.
           </p>
         </Card>
       </div>
       <div className="pr-24 flex flex-row-reverse w-full">
         <Card title="Apa kesimpulannya?">
           <p className="text-pretty text-sm">
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea nam voluptatem inventore asperiores optio ullam maxime quisquam et! Modi ipsa sed tempora perferendis, provident tenetur necessitatibus, nostrum inventore consequatur mollitia, sequi officiis! Perferendis, nihil quam natus nesciunt aperiam dolores quaerat sunt mollitia debitis ab quo deleniti nobis facere repudiandae minus?
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea nam voluptatem inventore asperiores optio ullam maxime quisquam et! Modi ipsa sed tempora perferendis, provident tenetur necessitatibus, nostrum inventore consequatur mollitia, sequi officiis! Perferendis, nihil quam natus nesciunt aperiam dolores quaerat sunt mollitia debitis ab quo deleniti nobis facere repudiandae minus?
            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea nam voluptatem inventore asperiores optio ullam maxime quisquam et! Modi ipsa sed tempora perferendis, provident tenetur necessitatibus, nostrum inventore consequatur mollitia, sequi officiis! Perferendis, nihil quam natus nesciunt aperiam dolores quaerat sunt mollitia debitis ab quo deleniti nobis facere repudiandae minus?
           </p>
         </Card>
